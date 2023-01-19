7f669e7d36661267f4fa6833279119c1645389e1
Tejus <tejus@bitqit.com>
2023-01-19 18:03:52 +0530

    Changes Requested

--- src/controllers/BankController.ts
+++ src/controllers/BankController.ts
@@ -4,14 +4,21 @@ import {BaseController} from "./BaseController";
 
 export class BankController extends BaseController {
     //create new bank
-    public async create(ctx: any) {
+
+    /**
+     * todo: use relevant name
+     * @param ctx
+     */
+    async create(ctx: any) {
         try {
             const bankRepository = connection.getRepository(Bank);
             const data: any = ctx.request['body'];
+            // todo: you can use type string[]
             const inputFields: any[] = ['name', 'address'];
             const error: any = {};
 
             inputFields.forEach(field => {
+                // todo: you may use "if(!data[field])"
                 if ((!data.hasOwnProperty(field)) || data[field] == "") {
                     try {
                         error[field] = `${field} is required`;
@@ -19,12 +26,12 @@ export class BankController extends BaseController {
                     }
                 }
             });
-
             if (JSON.stringify(error) !== '{}') {
                 return await super.response(ctx, 400, error);
             }
 
             await bankRepository.save(data);
+            // todo: try calling constructor and use "this"
             return await super.response(ctx, 201, "Bank created successfully");
         } catch (e: any) {
             console.log(e);
@@ -34,10 +41,12 @@ export class BankController extends BaseController {
 
     // update bank by bankId
 
+    // todo: rename updateBankById
     public async update(ctx: any) {
         try {
             const bankRepository = connection.getRepository(Bank);
             const data: any = ctx.request['body'];
+            // todo: use variable i.e bandId
             data.id = ctx.params.id;
             const inputFields: any[] = ['name', 'address'];
             const error: any = {};
@@ -51,6 +60,7 @@ export class BankController extends BaseController {
             });
             const checkId = await bankRepository.find({where: {id: data.id}});
             console.log(checkId);
+            // todo: you can do with array.length
             if (JSON.stringify(checkId) === '[]') {
                 error['notExist'] = "this bank does not exist";
             }
@@ -84,6 +94,7 @@ export class BankController extends BaseController {
         try {
             const bankRepository = connection.getRepository(Bank);
             const id = ctx.params.id;
+            // todo: this can be return and checked both at same time
             const checkId = await bankRepository.find({where: {id: id}});
             const error: any = {};
 
@@ -94,7 +105,7 @@ export class BankController extends BaseController {
             if (JSON.stringify(error) !== '{}') {
                 return await super.response(ctx, 400, error);
             }
-
+            // todo: no need to use connection
             return ctx.body = await connection.getRepository(Bank).findOneOrFail({where: {id: id}});
 
         } catch (e) {
